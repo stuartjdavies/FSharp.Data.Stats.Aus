3b4588607f24e748c0172b34dfc9e3d7aa24d83d
stuartjdavies <stuart_j_davies@hotmail.com>
2015-03-13 14:53:13 +1100

    Added simple calcuations on data set

--- FSharp.Data.Stats.Aus.AEMO/PriceAndDemandGeneralStats.fsx
+++ FSharp.Data.Stats.Aus.AEMO/PriceAndDemandGeneralStats.fsx
@@ -22,26 +22,38 @@ let gePriceAndDemandRows() =
 let getRegions (items : PriceAndDemandRecord seq) =
                 items |> Seq.map(fun item -> item.Region)
                       |> Seq.distinct
+                      |> Seq.toArray
 
 let getTotalDemand (items : PriceAndDemandRecord seq) =
         items |> Seq.sumBy(fun item -> item.TotalDemand)
 
-let getTotalDemandForEachRegion (rs : PriceAndDemandRecord seq) =      
+let getTotalDemandMin (items : PriceAndDemandRecord seq) =
+        items |> Seq.minBy(fun item -> item.TotalDemand)
+
+let getTotalDemandMax (items : PriceAndDemandRecord seq) =
+        items |> Seq.maxBy(fun item -> item.TotalDemand)
+
+let getTotalDemandAvg (items : PriceAndDemandRecord seq) =
+        items |> Seq.averageBy(fun item -> item.TotalDemand)
+
+let runCalcForEachRegion calc (rs : PriceAndDemandRecord seq) =      
         rs |> Seq.groupBy(fun r -> r.Region)
-           |> Seq.map(fun (r, rs) -> r, getTotalDemand(rs))          
-       
-let getTotalDemandForEachRegionAndYear (rs : PriceAndDemandRecord seq) =      
+           |> Seq.map(fun (r, rs) -> r, calc rs)          
+           |> Seq.toArray
+
+let runCalcForEachRegionAndYear calc (rs : PriceAndDemandRecord seq) =      
         let sumByYear (items : PriceAndDemandRecord seq) =
                 items |> Seq.groupBy(fun item -> item.SettlementDate.Year)
-                      |> Seq.map(fun (y, xs) -> y, getTotalDemand xs) 
+                      |> Seq.map(fun (y, xs) -> y, calc xs) 
         
         rs |> Seq.groupBy(fun item -> item.Region)
            |> Seq.map(fun (r, items) -> r, sumByYear items |> Seq.toArray)           
+           |> Seq.toArray
 
-let getTotalDemandForEachRegionYearAndMonth (rs : PriceAndDemandRecord seq) =      
+let runCalcForEachRegionYearAndMonth calc (rs : PriceAndDemandRecord seq) =      
         let sumByMonth (items : PriceAndDemandRecord seq) =
                items |> Seq.groupBy(fun item -> item.SettlementDate.Month)
-                     |> Seq.map(fun (m, xs) -> m, getTotalDemand xs) 
+                     |> Seq.map(fun (m, xs) -> m, calc xs) 
                      |> Seq.toArray
                        
         let groupByYear (items : PriceAndDemandRecord seq) =
@@ -50,14 +62,17 @@ let getTotalDemandForEachRegionYearAndMonth (rs : PriceAndDemandRecord seq) =
                       |> Seq.toArray
                                       
         rs |> Seq.groupBy(fun item -> item.Region)
-           |> Seq.map(fun (r, items) -> r, groupByYear items)           
-          
+           |> Seq.map(fun (r, items) -> r, groupByYear items)
+           |> Seq.toArray      
+                    
 let rows = gePriceAndDemandRows()
-let regions = getRegions rows |> Seq.toArray
-let totalDemandForEachRegion = getTotalDemandForEachRegion rows |> Seq.toArray
-let totalDemandForEachRegionAndYear = getTotalDemandForEachRegionAndYear rows |> Seq.toArray
-let totalDemandForEachRegionYearAndMonth = getTotalDemandForEachRegionYearAndMonth rows |> Seq.toArray
+let regions = getRegions rows 
+let totalDemandForEachRegion = rows |> runCalcForEachRegion getTotalDemand 
+let totalDemandForEachRegionAndYear = rows |> runCalcForEachRegionAndYear getTotalDemand 
+let totalDemandForEachRegionYearAndMonth = rows |> runCalcForEachRegionYearAndMonth getTotalDemand
+let avgDemandForEachRegionYearAndMonth = rows |> runCalcForEachRegionYearAndMonth getTotalDemandAvg
 
+// Windows
 
 
 
